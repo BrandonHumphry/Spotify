7e48c7753b9268eb9ac99418223788fe039c0e9b
Brandon Humphry <brandonhumphry@gmail.com>
2020-10-31 11:20:07 -0700

    installed spotify-web-api-js to connect with Spotify, user data pulled successfully

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+// just an instance to reuse shell from SpotifyApi
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -17,6 +21,12 @@ function App() {
 
     if (_token) {
       setToken(_token);
+      // below will give access token to react and Spotify API
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log(user);
+      });
     }
 
     console.log("I HAVE A TOKEN", token);
