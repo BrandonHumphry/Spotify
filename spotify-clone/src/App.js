90eb73f78084e1f267b9be0a59d5023c983a9b19
Brandon Humphry <brandonhumphry@gmail.com>
2020-11-02 23:28:29 -0800

    used dispatch to get user info into DataLayer

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -4,12 +4,14 @@ import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
 import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
 
 // just an instance to reuse shell from SpotifyApi
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  // use user from data layer
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   // run code based on a given condition
   useEffect(() => {
@@ -21,17 +23,29 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token
+      });
+
       // below will give access token to react and Spotify API
       spotify.setAccessToken(_token);
 
       spotify.getMe().then(user => {
         console.log(user);
+
+        dispatch({
+          type: "SET_USER",
+          user: user
+        });
       });
     }
 
     console.log("I HAVE A TOKEN", token);
   }, []);
+
+  console.log("USER", user);
+  console.log("TOKEN", token);
   //  if we have a token, render the player page, if not, go to Login
   return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
--- spotify-clone/src/reducer.js
+++ spotify-clone/src/reducer.js
@@ -2,21 +2,30 @@ export const initialState = {
   user: null,
   playlists: [],
   playing: false,
-  item: null
+  item: null,
+  // remove token before pushing app
+  token:
+    "BQB7vn309F0yVizW2lVjty87iFeNgb2vEpLDErmbCp0dN_7oIkmLDN-RYwD0R_JgoPYq3eKcnWNfikGHXBHlHIUybEDom4VNJXxEX9xkUPzt7mKNyBP5ETqjve3VsoQdxI3bbua3F1G4DxXbxYcwPYTVLdVur-c_rqd5YJA3UhlgRiDg"
 };
 
 // state = how it currently looks, action = set the user, currently playing, etc
 const reducer = (state, action) => {
-  // console log action and note it
+  // console log action and note it, this will help you debug in the future
   console.log(action);
 
   //   dispatch an action when the user does something i.e. set user when they log in
   switch (action.type) {
     case "SET_USER":
-      //   when you receive set user action, return below..spread state to keep old but give new state
+      //   when you receive set user action, return below..spread state to keep old but update user slice with action type
       return {
         ...state,
         user: action.user
+      };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token
       };
     // return original state if the action above doesn't happen
     default:
