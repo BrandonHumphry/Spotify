bd5103474cc51e9ef52f9e2644e9b2e12413d92e
Brandon Humphry <brandonhumphry@gmail.com>
2020-11-05 23:34:15 -0800

    added material UI icons and styled them in navigation

--- spotify-clone/src/SidebarOption.js
+++ spotify-clone/src/SidebarOption.js
@@ -4,7 +4,9 @@ import "./SidebarOption.css";
 function SidebarOption({ title, Icon }) {
   return (
     <div className="sidebarOption">
-      <p>{title}</p>
+      {Icon && <Icon className="sidebarOption__icon"></Icon>}
+      {/* if we have an icon, render it in h4, otherwise render it in a paragraph title */}
+      {Icon ? <h4>{title}</h4> : <p>{title}</p>}
     </div>
   );
 }
